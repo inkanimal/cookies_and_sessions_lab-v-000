7d7018a70a2792bb76ab6d6eb0bd04d2f11e69ad
geoffrey woodcock <theatomproject@gmail.com>
2018-11-17 21:36:35 +0000

    automatically backed up by learn

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,4 +1,11 @@
 class ProductController < ApplicationController
   
+  def add_to_cart
+  # Get the item from the path
+  @item = Item.find(params[:id])
+ 
+  # Load the cart from the session, or create a new empty cart.
+  current_cart << @item.id
+end
   
 end 
