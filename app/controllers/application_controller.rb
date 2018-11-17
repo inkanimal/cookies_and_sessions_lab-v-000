bab5458f05754271decfb58cedfe716a7e6ac552
geoffrey woodcock <theatomproject@gmail.com>
2018-11-17 21:37:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-   helper_method :current_cart
  
-  def current_cart
-    session[:cart] ||= []
-  end
-end
 end
